4fa324ead6cd4f1e289b438f4d871538c7877ef9
Marcel Hase <139238079+P2YCH0@users.noreply.github.com>
2023-08-11 09:01:06 +0200

    Das ist der zweite commit

--- main.dart
+++ main.dart
@@ -1,21 +1,9 @@
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(BlabbersApp());
-  final blabbers = Blabbers();
-  blabbers.postBlabb('user1', 'Hallo, das ist mein erster Tweet!');
-  blabbers.postBlabb('user2', 'Schön, dich zu treffen! #Willkommen');
-  blabbers.postBlabb('user1', 'Danke! #Aufgeregt');
-  final user1Blabbs = blabbers.getTweetsByUsername('user1');
-  final timeline = blabbers.getTimeline();
-  print("Blabbs from User1:");
-  for (var blabb in user1Blabbs) {
-    print('${blabb.username} (${blabb.timestamp}): ${blabb.content}');
-  }
-  print('\nTimeline:');
-  for (var blabb in timeline) {
-    print('${blabb.username} (${blabb.timestamp}): ${blabb.content}');
-  }
 }
+
 class BlabbersApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -28,32 +16,108 @@ class BlabbersApp extends StatelessWidget {
     );
   }
 }
-class BlabbersHomePage extends StatelessWidget {
+
+class BlabbersHomePage extends StatefulWidget {
+  @override
+  _BlabbersHomePageState createState() => _BlabbersHomePageState();
+}
+
+class _BlabbersHomePageState extends State<BlabbersHomePage> {
+  int _thumbUpCount = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // APP BAR
       appBar: AppBar(
-        title: Image.asset("assets/images/Logo.png", fit: BoxFit.cover),
-       leading: const Icon(
-          Icons.account_circle_rounded,
-          size: 36,
+  centerTitle: true,
+  title: Image.asset(
+    "assets/images/Logo.png",
+    fit: BoxFit.contain,
+    height: 30,
+  ),
+  backgroundColor: Colors.transparent,
+  elevation: 0,
+  iconTheme: IconThemeData(color: Colors.green),
+  bottom: PreferredSize(
+    preferredSize: Size.fromHeight(4.0), 
+    child: Container(
+      color: Colors.green, 
+    ),
+  ),
+),
+    
+    
+      body: SafeArea(
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          child: ListView.builder(
+            itemCount: 1,
+            itemBuilder: (context, index) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      children: [
+                        CircleAvatar(
+                          radius: 30,
+                          backgroundImage:
+                              AssetImage("assets/images/profile.jpeg"),
+                        ),
+                        SizedBox(width: 16),
+                        Text(
+                          "Benutzername",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Image.asset(
+                    "assets/images/nachrichten2.jpeg",
+                    fit: BoxFit.cover,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Dies ist ein Beispieltext, der auf 160 Zeichen begrenzt ist. Dies ist ein Beispieltext, der auf 160 Zeichen begrenzt ist.',
+                      maxLines: 4,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                    child: FittedBox(
+                      fit: BoxFit.fitHeight,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          _buildCountButton(Icons.thumb_up, Colors.green),
+                          Text(
+            '$_thumbUpCount',
+            style: TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+                          _buildRoundedButton(Icons.thumb_down, Colors.green, () {}),
+                          _buildRoundedButton(Icons.comment, Colors.green, () {}),
+                          _buildRoundedButton(Icons.send, Colors.green, () {}),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+          ),
         ),
-        leadingWidth: 100,
-        iconTheme: const IconThemeData(
-          color: Colors.black38,
-        ),
-        
-        backgroundColor: Colors.white,
       ),
-      body: const Center(
-        child: Text(
-          'BLABBERS',
-          style: TextStyle(
-              fontSize: 36, color: Colors.green, fontWeight: FontWeight.bold),
-        ),
-      ),
-      // DRAWER
       drawer: Drawer(
         child: ListView(
           padding: EdgeInsets.zero,
@@ -90,42 +154,42 @@ class BlabbersHomePage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) =>  NewPage())
+            context,
+            MaterialPageRoute(builder: (context) => PostPage()),
           );
         },
         child: const Icon(Icons.add),
         backgroundColor: Colors.green[600],
       ),
       bottomNavigationBar: BottomAppBar(
-        color: Colors.white,
+        color: Colors.green,
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceAround,
           children: <Widget>[
             IconButton(
               icon: const Icon(Icons.home),
-              color: Colors.black38,
+              color: Colors.white,
               onPressed: () {
                 // Implement action for Home Button
               },
             ),
             IconButton(
               icon: const Icon(Icons.search),
-              color: Colors.black38,
+              color: Colors.white,
               onPressed: () {
                 // Implement action for Message Button
               },
             ),
             IconButton(
               icon: const Icon(Icons.notifications),
-              color: Colors.black38,
+              color: Colors.white,
               onPressed: () {
                 // Implement action for Notifications Button
               },
             ),
             IconButton(
               icon: const Icon(Icons.email),
-              color: Colors.black38,
+              color: Colors.white,
               onPressed: () {},
             )
           ],
@@ -133,67 +197,97 @@ class BlabbersHomePage extends StatelessWidget {
       ),
     );
   }
-}
-class Blabb {
-  final String username;
-  final String content;
-  final DateTime timestamp;
-  Blabb(this.username, this.content, this.timestamp);
-}
-class Blabbers {
-  List<Blabb> _blabbs = [];
-  void postBlabb(String username, String content) {
-    final timestamp = DateTime.now();
-    final tweet = Blabb(username, content, timestamp);
-    _blabbs.add(tweet);
+
+  Widget _buildRoundedButton(IconData icon, Color color, VoidCallback onPressed) {
+    return MaterialButton(
+      onPressed: onPressed,
+      color: color,
+      shape: CircleBorder(),
+      child: Icon(
+        icon,
+        color: Colors.white,
+        size: 20,
+      ),
+    );
   }
-  List<Blabb> getTweetsByUsername(String username) {
-    return _blabbs.where((tweet) => tweet.username == username).toList();
-  }
-  List<Blabb> getTimeline() {
-    return _blabbs.reversed.toList();
-  }
-}
-class NewPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
+
+  Widget _buildCountButton(IconData icon, Color color) {
+    return MaterialButton(
+      onPressed: () {
+        setState(() {
+          if (_thumbUpCount > 0) {
+            _thumbUpCount--;
+          } else {
+            _thumbUpCount++;
+          }
+        });
+      },
+      color: color,
+      shape: CircleBorder(),
+      child: Row(
         children: [
+          Icon(
+            icon,
+            color: Colors.white,
+            size: 20,
+          ),
+          SizedBox(width: 4),
           
-          FloatingActionButton(
-        child: Icon(
-          Icons.
-        ),
-        onPressed: () {
-          //...
-        },
-        heroTag: null,
-      ),
-      SizedBox(
-        height: 10,
-      ),
-      FloatingActionButton(           
-        child: Icon(
-          Icons.star
-        ),
-        onPressed: () {},
-        heroTag: null,
-      )
         ],
       ),
     );
   }
 }
 
+class PostPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        leading: IconButton(
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+          icon: Icon(Icons.arrow_back),
+          color: Colors.white,
+        ),
+      ),
+      body: GestureDetector(
+        onTap: () {
+          Navigator.of(context).pop();
+        },
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              _buildRoundedButton(Icons.thumb_up, Colors.green, () {}),
+              _buildRoundedButton(Icons.comment, Colors.green, () {}),
+              _buildRoundedButton(Icons.send, Colors.green, () {}),
+              SizedBox(height: 16),
+              FloatingActionButton(
+                onPressed: () {},
+                child: Icon(Icons.create),
+                backgroundColor: Colors.green[600],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
-
-
-
-
-
-
-
+  Widget _buildRoundedButton(IconData icon, Color color, VoidCallback onPressed) {
+    return MaterialButton(
+      onPressed: onPressed,
+      color: color,
+      shape: CircleBorder(),
+      child: Icon(
+        icon,
+        color: Colors.white,
+        size: 20,
+      ),
+    );
+  }
+}
